a9048fc71a107d1a3e96756317929728710c281c
John Need <johnneed@johnneed.com>
2019-02-04 06:28:49 -0500

    more pics

--- kitten-roulette-redux-thunk/screens/home/actions.js
+++ kitten-roulette-redux-thunk/screens/home/actions.js
@@ -15,7 +15,6 @@ export function attemptToCuddle() {
             }
 
             if (response === 'scratch') {
-
                 // Scratch
                 dispatch({
                     type: SCRATCH
